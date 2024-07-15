42d77384dd3c4cb03abca60c2764d65b2a43abdf
jdesh19 <jarondesharnais@gmail.com>
2024-07-15 17:40:56 -0500

    added a show page for tee times.

--- app/controllers/tee_times_controller.rb
+++ app/controllers/tee_times_controller.rb
@@ -2,4 +2,8 @@ class TeeTimesController < ApplicationController
   def index
     @tee_times = TeeTime.page(params[:page]).per(49)
   end
+
+  def show
+    @tee_time = TeeTime.find(params[:id])
+  end
 end
